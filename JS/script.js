7dbbfc1b9e213b092e51764b3870eb2e40657e4e
Fahad Warsi <99warsifahad@gmail.com>
2026-01-10 17:07:33 +0530

    update-6

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+function sendMail(){
+    let parms = {
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        subject : document.getElementById("subject").value,
+        message : document.getElementById("message").value,
+    }
+
+    emailjs.send("service_ssb3jbe","template_446l4zt",parms).then(alert("Email Sent!!"))
+}
